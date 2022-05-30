c6ad451d49c4a6f5b0b616eb744f0040bc68afba
Kate Bonner <katebonner277@gmail.com>
2022-05-30 14:55:53 -0400

    starting heatmap work

--- assets/script.js
+++ assets/script.js
@@ -908,25 +908,26 @@ var framePosLocal = JSON.parse(framePos);
     new Commutes(CONFIGURATION);
   }
 
+
 // // HEAT MAP TEST
 // /* Data points defined as a mixture of WeightedLocation and LatLng objects */
-// var heatMapData = [
-//     {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
-//     new google.maps.LatLng(37.782, -122.445),
-//     {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
-//     {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
-//     {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
-//     new google.maps.LatLng(37.782, -122.437),
-//     {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
+var heatMapData = [
+    {location: new google.maps.LatLng(37.782, -122.447), weight: 30},
+    new google.maps.LatLng(37.782, -122.445),
+    {location: new google.maps.LatLng(37.782, -122.443), weight: 20},
+    {location: new google.maps.LatLng(37.782, -122.441), weight: 32},
+    {location: new google.maps.LatLng(37.782, -122.439), weight: 22},
+    new google.maps.LatLng(37.782, -122.437),
+    {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
   
-//     {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
-//     {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
-//     new google.maps.LatLng(37.785, -122.443),
-//     {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
-//     new google.maps.LatLng(37.785, -122.439),
-//     {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
-//     {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
-//   ];
+    {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
+    {location: new google.maps.LatLng(37.785, -122.445), weight: 24},
+    new google.maps.LatLng(37.785, -122.443),
+    {location: new google.maps.LatLng(37.785, -122.441), weight: 5},
+    new google.maps.LatLng(37.785, -122.439),
+    {location: new google.maps.LatLng(37.785, -122.437), weight: 10},
+    {location: new google.maps.LatLng(37.785, -122.435), weight: 50}
+  ];
   
 //   var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
   
@@ -936,7 +937,7 @@ var framePosLocal = JSON.parse(framePos);
 //     mapTypeId: 'satellite'
 //   });
   
-//   var heatmap = new google.maps.visualization.HeatmapLayer({
-//     data: heatMapData
-//   });
-//   heatmap.setMap(map);
+  var heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatMapData
+  });
+  heatmap.setMap(commutesMap);
